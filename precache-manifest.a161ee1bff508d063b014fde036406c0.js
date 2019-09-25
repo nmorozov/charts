21ea0c00d91767039584d8a8ca8edb8d641f0e75
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-25 06:00:56 +0000

    Deploy nmorozov/charts to github.com/nmorozov/charts.git:gh-pages

---
+++ precache-manifest.a161ee1bff508d063b014fde036406c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941b6b35ebd95f3478873b71fd299b92",
+    "url": "/charts/index.html"
+  },
+  {
+    "revision": "639db10f007c3462cece",
+    "url": "/charts/static/css/main.f65a4e3a.chunk.css"
+  },
+  {
+    "revision": "5b126e44910cd5c34579",
+    "url": "/charts/static/js/2.6e20a562.chunk.js"
+  },
+  {
+    "revision": "639db10f007c3462cece",
+    "url": "/charts/static/js/main.f4d0669a.chunk.js"
+  },
+  {
+    "revision": "70495989b4bfc2a715b4",
+    "url": "/charts/static/js/runtime~main.3df34662.js"
+  }
+]);
